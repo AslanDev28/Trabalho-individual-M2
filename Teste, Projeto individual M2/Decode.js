f15bc961550542a7a4b572e2fb182c1b74baf5f0
Aslan Campos <113717097+AslanDev28@users.noreply.github.com>
2022-10-16 22:00:22 -0300

    Add files via upload

---
+++ Teste, Projeto individual M2/Decode.js
@@ -0,0 +1,13 @@
+
+
+function clicar(){
+    var btn1 = document.getElementById('btn1')
+    var cdc = document.querySelector('div.cdc')
+
+    if(btn1 = clicar){
+        document.querySelector('div.cdc').style.display = 'none'
+        console.log("MEU PAU")
+    } 
+   
+}
+
